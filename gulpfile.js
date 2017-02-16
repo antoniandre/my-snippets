3be1f8af845cd08ea1e3589ba5708042c286cfde
Antoni Andre <antoniandre.web@gmail.com>
2017-02-16 18:13:45 +0800

    Improve gulp workflow add bower files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
+/**
+ * Use in terminal
+ * ---------------
+ *
+ * gulp bower
+ * gulp dev
+ */
+
 var gulp    = require('gulp'),
     // All the devDependencies from package.json.
     plugins = require('gulp-load-plugins')(
     {
-        pattern: ['gulp-*', 'gulp.*', 'main-bower-files', 'browser-sync'],
+        pattern: ['gulp-*', 'main-bower-files', 'browser-sync', 'merge2'],
         replaceString: /\bgulp[\-.]/
     }),
     config  =
@@ -11,7 +19,8 @@ var gulp    = require('gulp'),
         src : './src',// Dev.
         dest: './dist',// Distribution.
         bowerDir: './bower_components' 
-    };
+    },
+    bowerFiles;
 
 gulp
     // INDIVIDUAL TASKS.
@@ -21,74 +30,73 @@ gulp
     .task('create-bower', function()
     { 
         return plugins.bower().pipe(gulp.dest(config.bowerDir)) ;
+        console.log('OK - Bower folder generated.');
     })
-    
+
     // Php and html.
     .task('php', function()
     {
         gulp.src(config.src + '/**/*.+(php|html)')
             .pipe(gulp.dest(config.dest));
-        console.log('Php files copied.');
+        console.log('OK - PHP / HTML files copied.');
     })
 
     // Css.
-    .task('sass', function()
+    /*.task('sass', function()
     {
         gulp.src(config.src + '/css/*.scss')
-            .pipe(plugins.sass(
-            {
-                 // style: 'compressed',
-                 /*loadPath:
-                [
-                     './resources/sass',
-                     config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                     config.bowerDir + '/fontawesome/scss'
-                 ]*/
-             }))
+            .pipe(plugins.sass())
             .pipe(plugins.cssbeautify())
             .pipe(plugins.autoprefixer())
             .pipe(gulp.dest(config.dest + '/css/'))
             .pipe(plugins.browserSync.reload({stream: true}));
 
-        console.log('Css compiling task completed.');
-    })
-
-    .task('minify-css', function()
+        console.log('OK - CSS compiling task completed.');
+    })*/
+    .task('css', function()
     {
-        gulp.src(config.dest + '/css/*.css')
-            .pipe(plugins.uglify())
-            .pipe(plugins.rename({suffix: '.min'}))
+        gulp.src(getFiles('scss'))
+           .pipe(plugins.sass())
+           .pipe(plugins.autoprefixer())
+           .pipe(plugins.cssbeautify())
+           .pipe(gulp.dest(config.dest + '/css/'));
+
+        gulp.src(getFiles('css'))
+           .pipe(plugins.autoprefixer())
+           .pipe(plugins.cssbeautify())
+           .pipe(gulp.dest(config.dest + '/css/'));
+
+            console.log('OK - SCSS/CSS compiling task completed.');
+    })
+    .task('css-min', function()
+    {
+        var scss = gulp.src(getFiles('scss'))
+                       .pipe(plugins.sass()),
+            css  = gulp.src(getFiles('css'));
+
+            plugins.merge2(scss, css)
+            .pipe(plugins.autoprefixer())
+            .pipe(plugins.csso())
+            .pipe(plugins.concat('main.min.css'))
             .pipe(gulp.dest(config.dest + '/css/'));
-        console.log('Css minifying task completed.');
+        console.log('OK - CSS minifying task completed.');
     })
 
     // Js.
     .task('js', function()
     {
-        gulp.src(config.src + '/js/*.js')
-               .pipe(gulp.dest(config.dest + '/js/'));
-        console.log('JS files copied.');
-    })
-
-    .task('minify-js', function()
-    {
-        gulp.src(config.dest + '/js/*.js')
-            .pipe(plugins.rename({suffix: '.min'}))
+        gulp.src(getFiles('js'))
             .pipe(gulp.dest(config.dest + '/js/'));
-        console.log('JS minifying task completed.');
+        console.log('OK - JS files copied.');
     })
-
     // Concatenates all the JS and the bower js libs.
-    .task('merge-js', function() {
-
-        var jsFiles = [config.src + '/js/*'];
-
-        gulp.src(plugins.mainBowerFiles().concat(jsFiles))
-            .pipe(plugins.filter('**/*.js'))
-            .pipe(plugins.concat('main.js'))// Concat all the files into main.js.
-            .pipe(plugins.rename({suffix: '.min'}))
+    .task('js-min', function()
+    {
+        gulp.src(getFiles('js'))
+            .pipe(plugins.concat('main.min.js'))
             .pipe(plugins.uglify())
             .pipe(gulp.dest(config.dest + '/js/'));
+        console.log('OK - JS minifying task completed.');
     })
 
 
@@ -120,3 +128,37 @@ gulp
     .task('dev', ['build', 'watch'])
     .task('prod', ['build', 'minify-css', 'minify-js'])
     .task('default', ['dev']);// Run when typing 'gulp' (only) in console.
+
+
+
+/**
+ * Get CSS or JS Files.
+ *
+ * @param  string type: CSS, SCSS or JS.
+ * @return array: the array of matched files paths.
+ */
+function getFiles(type)
+{
+    // Type files mask CSS or JS.
+    var dir        = type === 'scss' ? 'css' : type,
+        files      = [config.src + '/' + dir + '/*.+' + '(' + type + ')'],
+        filter     = new RegExp('\.' + type + '$'),
+        bowerFiles = getBowerFiles();
+
+    if (bowerFiles) files = bowerFiles.filter(function(path){return filter.test(path)}).concat(files);
+
+    return files;
+}
+
+function getBowerFiles()
+{
+    var files = null;
+
+    if (bowerFiles) return bowerFiles;
+
+    try {files = plugins.mainBowerFiles()}
+    catch(e) {console.log('============= /!\\ =============\n' + e + '\n===============================\nIgnoring bower files...')}
+
+    bowerFiles = files;
+    return files;
+}
